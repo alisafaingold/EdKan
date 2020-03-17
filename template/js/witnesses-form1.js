cf9b4fb5e47c305dcf3c450c6e1d718d73e9e408
alisafa <nsq8seWe>
2020-03-17 16:55:12 +0200

    add case

--- template/js/witnesses-form1.js
+++ template/js/witnesses-form1.js
@@ -1,5 +1,12 @@
+document.getElementById('datetimepicker').onclick=function () {
+    datepi
+}
+
+
 var lawyerName = document.getElementById("lawyerName");
 
 var userName = document.getElementById("userName").innerText;
 
 lawyerName.setAttribute('value',userName)
+
+
